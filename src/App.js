47fdf8784c087c178346a1079360d7246548e221
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-21 19:13:51 +0200

    Add Router to index.js and Switch to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Switch } from "react-router-dom"
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Switch></Switch>
+    </div>
+  )
 }
 
 export default App
