fa4b671b36d0f7e34ccb5eecb0f983ccbd05e65d
Rafael-Fontes-Baptista <rafaelfontesb@gmail.com>
2021-06-21 20:00:47 +0200

    Create and configure redux store, add appState reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { Switch, Route } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { selectAppLoading } from "./store/appState/selectors"
+import Loading from "./components/Loading"
 import SignupPage from "./pages/User/SignupPage"
 import LoginPage from "./pages/User/LoginPage"
 import "./App.css"
 
 function App() {
+  const isLoading = useSelector(selectAppLoading)
   return (
     <div className="App">
+      {isLoading ? <Loading /> : null}
       <Switch>
         <Route path="/signup" component={SignupPage} />
         <Route path="/login" component={LoginPage} />
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
+import store from "./store"
+import { Provider } from "react-redux"
 import { BrowserRouter as Router } from "react-router-dom"
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 )
